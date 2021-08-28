acf5b3b965a21d577cb2391e8356246d69c828f0
joeycool99 <72965762+joeycool99@users.noreply.github.com>
2021-08-28 21:52:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var h1 = document.getElementById("timer")
 
-const delayedColorChange = (time, delay, doNext) => {
+const delayedChange = (time, delay, doNext) => {
     setTimeout(() => {
         console.log(typeof (time))
 
@@ -10,17 +10,17 @@ const delayedColorChange = (time, delay, doNext) => {
 }
 
 
-delayedColorChange('10', 1000, () => {
-    delayedColorChange('9', 1000, () => {
-        delayedColorChange('8', 1000, () => {
-            delayedColorChange('7', 1000, () => {
-                delayedColorChange('6', 1000, () => {
-                    delayedColorChange('5', 1000, () => {
-                        delayedColorChange('4', 1000, () => {
-                            delayedColorChange('3', 1000, () => {
-                                delayedColorChange('2', 1000, () => {
-                                    delayedColorChange('1', 1000, () => {
-                                        delayedColorChange(' Happy Independence Day ', 1000, () => {
+delayedChange('10', 1000, () => {
+    delayedChange('9', 1000, () => {
+        delayedChange('8', 1000, () => {
+            delayedChange('7', 1000, () => {
+                delayedChange('6', 1000, () => {
+                    delayedChange('5', 1000, () => {
+                        delayedChange('4', 1000, () => {
+                            delayedChange('3', 1000, () => {
+                                delayedChange('2', 1000, () => {
+                                    delayedChange('1', 1000, () => {
+                                        delayedChange(' Happy Independence Day ', 1000, () => {
                                             h1.setAttribute("style", "padding-top:120px;text-align:center")
                                         })
                                     })
